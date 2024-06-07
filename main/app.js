298c83840b8668e130ea4871d97c409c21268190
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-06-07 15:44:37 +0500

    javascript

---
+++ main/app.js
@@ -0,0 +1,169 @@
+// {
+//     // 1 masala
+//     let a = +prompt('a son kiriting')
+//     if (a > 0) {
+//         ++a
+//         console.log(a);
+//     }
+//     else{
+//         console.log(a);
+//     }
+// }
+
+// {
+//     // 2 masala
+//     let number = +prompt('son kiriting')
+//     if (number > 0) {
+//         ++number
+//         console.log(number);
+//     }
+//     else if (number < 0 ) {
+//         number -= 2
+//         console.log(number);
+//     }
+// }
+
+// {
+//     // masala 3
+//     let number = +prompt('son kiriting')
+//     if (number > 0) {
+//         ++number
+//         console.log(number);
+//     }
+//     else if (number < 0) {
+//         number -= 2
+//         console.log(number);
+//     }
+//     else{
+//         number === 0
+//         number = 10
+//         console.log(number);
+//     }
+// }
+
+// {
+//     // masala 4
+//     let a = +prompt('a son kiriting')
+//     let b = +prompt('b son kiriting')
+//     let c = +prompt('c son kiriting')
+//     let musbat = 0
+//     if (a > 0) {
+//         musbat++    
+//     }
+//     if (b > 0) {
+//         musbat++    
+//     }
+//     if (c > 0) {
+//         musbat++    
+//     }
+//     console.log(musbat);
+// }
+
+// {
+//     // masala 5
+//     let a = +prompt('a son kiriting');
+//     let b = +prompt('b son kiriting');
+//     let c = +prompt('c son kiriting');
+//     let musbat = 0
+//     let manfiy = 0
+//     if (a > 0) {
+//         musbat++
+//     }
+//     else{
+//         manfiy++
+//     }
+//     if (b > 0) {
+//         musbat++
+//     }
+//     else {
+//         manfiy++
+//     }
+
+//     if (c > 0) {
+//         musbat++
+//     }
+//     else {
+//         manfiy++
+//     }
+//     console.log(musbat);
+//     console.log(manfiy);
+
+// }
+
+// {
+//     // masala 6
+//     let a = +prompt('a son kiriting')
+//     let b = +prompt('b son kiriting')
+//     if (a > b) {
+//         console.log(katta son ${a});
+//     }
+//     else{
+//         console.log(katta son ${b});
+//     }
+// }
+
+// {
+//     // masala 7
+//     let a = +prompt('a son kiriting')
+//     let b = +prompt('b son kiriting')
+//     if (a > b) {
+//         console.log(kichik son ${b});
+//     }
+//     else if (b > a) {
+//         console.log(kichik son ${a});
+//     }
+
+// }
+
+// {
+//     // masala 8
+//     let a = +prompt('a son kiriting')
+// let b = +prompt('a son kiriting')
+// let kattasi,kichiki
+// if (a > b) {
+//     kattasi = a
+//     kichiki = b
+// }
+// else {
+//     kichiki = a
+//     kattasi = b
+// }
+// console.log(kattasi ${kattasi});
+// console.log(kichiki ${kichiki});
+// }
+
+// {
+//     // masala 8
+//     let a = +prompt('a son kiriting')
+// let b = +prompt('a son kiriting')
+// let kattasi,kichiki
+// if (a > b) {
+//     kattasi = b
+//     kichiki = a
+// }
+// else {
+//     kichiki = b
+//     kattasi = a
+// }
+// console.log(a ${kichiki});
+// console.log(b ${kattasi});
+
+// }
+
+
+
+// {
+//     // masala 10
+//     let a = +prompt("a sonini kirting")
+//     let b = +prompt("b sonini kirting")
+//     let sum = 0
+//     if(a!=b){
+//         sum = a + b
+//         alert(${sum})
+//     }else{
+//         a = 0
+//         b = 0
+//         alert(${a} ${b})
+//     }
+
+// }
